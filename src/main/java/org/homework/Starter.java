3b308f022514861446926f3d0eba56e03c514bfe
Tango <53480103+tango-del@users.noreply.github.com>
2021-03-13 10:10:30 +0200

    move different realization in methods

--- src/main/java/org/homework/Starter.java
+++ src/main/java/org/homework/Starter.java
@@ -13,20 +13,28 @@ import java.time.LocalDate;
 
 public class Starter {
     public static void main(String[] args) {
-
-        // поднимается контекст в рамках маленького bean factory который мало что умеет
-        final BeanFactory beanFactory = new XmlBeanFactory(new ClassPathResource("common-beans.xml"));
-
-        final ApplicationContext applicationContext = new ClassPathXmlApplicationContext("common-beans.xml");
-        // дай нам bean у которых объекты имеют тип как JourneyService
-        final JourneyService journeyService = applicationContext.getBean(JourneyService.class);
-//        final JourneyService journeyService = beanFactory.getBean(JourneyService.class);
-        System.out.println(journeyService.find("Odessa", "Kiev", LocalDate.now(), LocalDate.now().plusDays(1)));
-        System.out.println(journeyService.find("Odessa", "Kiev", LocalDate.now(), LocalDate.now().plusDays(6)));
-
+        appContextSpringInit();
     }
 
-    public static void exampleBeforeSpringContext() throws IOException, IllegalAccessException, InstantiationException, ClassNotFoundException {
+    private static void appContextSpringInit() {
+        final ApplicationContext applicationContext = new ClassPathXmlApplicationContext("common-beans.xml");
+
+        System.out.println("after init");
+        // дай нам bean у которых объекты имеют тип как JourneyService
+        final JourneyService journeyService = applicationContext.getBean(JourneyService.class);
+        System.out.println(journeyService.find("Odessa", "Kiev", LocalDate.now(), LocalDate.now().plusDays(1)));
+        System.out.println(journeyService.find("Odessa", "Kiev", LocalDate.now(), LocalDate.now().plusDays(6)));
+    }
+
+    private static void beanFactorySpringInit() {
+        // поднимается контекст в рамках маленького bean factory который мало что умеет
+        final BeanFactory beanFactory = new XmlBeanFactory(new ClassPathResource("common-beans.xml"));
+        final JourneyService journeyService = beanFactory.getBean(JourneyService.class);
+        System.out.println(journeyService.find("Odessa", "Kiev", LocalDate.now(), LocalDate.now().plusDays(1)));
+        System.out.println(journeyService.find("Odessa", "Kiev", LocalDate.now(), LocalDate.now().plusDays(6)));
+    }
+
+    private static void exampleBeforeSpringContext() throws IOException, IllegalAccessException, InstantiationException, ClassNotFoundException {
         AppContext.load("application.properties");
         final JourneyService journeyService = AppContext.getBean("journeyService");
 
