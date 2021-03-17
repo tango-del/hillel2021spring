1184692098f2e82c6b7762bc7265a6719b6f539f
Tango <53480103+tango-del@users.noreply.github.com>
2021-03-17 12:36:21 +0200

    update departure arrival in sql rows

--- src/main/resources/console.sql
+++ src/main/resources/console.sql
@@ -10,12 +10,12 @@ create table database_journey_service.journey
 	route varchar(200) not null
 );
 
-INSERT INTO database_journey_service.journey (station_from, station_to, departure, arrival, route) VALUES ('Odessa', 'Kiev', '2021-03-16', '2021-03-17', 'Odessa->Kiev');
 INSERT INTO database_journey_service.journey (station_from, station_to, departure, arrival, route) VALUES ('Odessa', 'Kiev', '2021-03-17', '2021-03-18', 'Odessa->Kiev');
 INSERT INTO database_journey_service.journey (station_from, station_to, departure, arrival, route) VALUES ('Odessa', 'Kiev', '2021-03-18', '2021-03-19', 'Odessa->Kiev');
-INSERT INTO database_journey_service.journey (station_from, station_to, departure, arrival, route) VALUES ('Kiev', 'Odessa', '2021-03-16', '2021-03-17', 'Kiev->Odessa');
+INSERT INTO database_journey_service.journey (station_from, station_to, departure, arrival, route) VALUES ('Odessa', 'Kiev', '2021-03-19', '2021-03-20', 'Odessa->Kiev');
 INSERT INTO database_journey_service.journey (station_from, station_to, departure, arrival, route) VALUES ('Kiev', 'Odessa', '2021-03-17', '2021-03-18', 'Kiev->Odessa');
 INSERT INTO database_journey_service.journey (station_from, station_to, departure, arrival, route) VALUES ('Kiev', 'Odessa', '2021-03-18', '2021-03-19', 'Kiev->Odessa');
-INSERT INTO database_journey_service.journey (station_from, station_to, departure, arrival, route) VALUES ('Lviv', 'Kiev', '2021-03-16', '2021-03-17', 'Lviv->Kiev');
+INSERT INTO database_journey_service.journey (station_from, station_to, departure, arrival, route) VALUES ('Kiev', 'Odessa', '2021-03-19', '2021-03-20', 'Kiev->Odessa');
 INSERT INTO database_journey_service.journey (station_from, station_to, departure, arrival, route) VALUES ('Lviv', 'Kiev', '2021-03-17', '2021-03-18', 'Lviv->Kiev');
 INSERT INTO database_journey_service.journey (station_from, station_to, departure, arrival, route) VALUES ('Lviv', 'Kiev', '2021-03-18', '2021-03-19', 'Lviv->Kiev');
+INSERT INTO database_journey_service.journey (station_from, station_to, departure, arrival, route) VALUES ('Lviv', 'Kiev', '2021-03-19', '2021-03-20', 'Lviv->Kiev');
