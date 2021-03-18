0c86b7493e76d50d9f95f1fb47a4d227ebc40ad4
tango-del <53480103+tango-del@users.noreply.github.com>
2021-03-18 18:37:44 +0200

    DatabaseConfig made DataSource, EntityManager and Transaction for Hibernate Configuration

--- src/main/java/org/homework/config/DatabaseConfig.java
+++ src/main/java/org/homework/config/DatabaseConfig.java
@@ -2,17 +2,26 @@ package org.homework.config;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import org.hibernate.dialect.PostgreSQL10Dialect;
 import org.postgresql.ds.PGSimpleDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
 @PropertySource("database.properties")
+@EnableTransactionManagement
 public class DatabaseConfig {
 
     @Autowired
@@ -29,5 +38,29 @@ public class DatabaseConfig {
         HikariDataSource dataSource = new HikariDataSource(config);
 
         return dataSource;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean emf(DataSource dataSource) {
+        LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
+        emf.setDataSource(dataSource);
+        emf.setPackagesToScan("org.homework.persistence.entity");
+        emf.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+
+        Properties properties = new Properties();
+        properties.put("hibernate.dialect", PostgreSQL10Dialect.class.getName());
+        properties.put("hibernate.hbm2ddl.auto", "create-drop");
+        properties.put("hibernate.show_sql", "true");
+
+        emf.setJpaProperties(properties);
+        return emf;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        JpaTransactionManager jpaTransactionManager = new JpaTransactionManager();
+        jpaTransactionManager.setEntityManagerFactory(entityManagerFactory);
+        jpaTransactionManager.setDataSource(dataSource());
+        return jpaTransactionManager;
     }
 }
