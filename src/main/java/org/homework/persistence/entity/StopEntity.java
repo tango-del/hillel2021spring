ff06e02757720a7d2533260dcf63c3303b793519
tango-del <53480103+tango-del@users.noreply.github.com>
2021-03-26 16:36:08 +0200

    add cascade PERSIST in StopEntity OneToOne

--- src/main/java/org/homework/persistence/entity/StopEntity.java
+++ src/main/java/org/homework/persistence/entity/StopEntity.java
@@ -16,6 +16,15 @@ public class StopEntity extends AbstractModifyEntity<Long> {
     @Embedded
     private CommonInfo commonInfo;
 
-    @OneToOne(mappedBy = "stop")
+    @OneToOne(mappedBy = "stop", cascade = CascadeType.PERSIST)
     private StopAdditionalInfoEntity additionalInfo;
+
+    public void addStopAdditionalInfo(final StopAdditionalInfoEntity stopAdditionalInfo) {
+        if (stopAdditionalInfo == null) {
+            this.additionalInfo = null;
+            return;
+        }
+        stopAdditionalInfo.setStop(this);
+        this.additionalInfo = stopAdditionalInfo;
+    }
 }
