3afed721c36edae54ac5ebdf6092248ac5b06009
tango-del <53480103+tango-del@users.noreply.github.com>
2021-03-26 12:31:04 +0200

    update sql

--- src/main/resources/console.sql
+++ src/main/resources/console.sql
@@ -8,12 +8,12 @@ create table journey
 	route varchar(200) not null
 );
 
-INSERT INTO journey (station_from, station_to, departure, arrival, route) VALUES ('Odessa', 'Kiev', '2021-03-24', '2021-03-25', 'Odessa->Kiev');
-INSERT INTO journey (station_from, station_to, departure, arrival, route) VALUES ('Odessa', 'Kiev', '2021-03-25', '2021-03-26', 'Odessa->Kiev');
 INSERT INTO journey (station_from, station_to, departure, arrival, route) VALUES ('Odessa', 'Kiev', '2021-03-26', '2021-03-27', 'Odessa->Kiev');
-INSERT INTO journey (station_from, station_to, departure, arrival, route) VALUES ('Kiev', 'Odessa', '2021-03-24', '2021-03-25', 'Kiev->Odessa');
-INSERT INTO journey (station_from, station_to, departure, arrival, route) VALUES ('Kiev', 'Odessa', '2021-03-25', '2021-03-26', 'Kiev->Odessa');
+INSERT INTO journey (station_from, station_to, departure, arrival, route) VALUES ('Odessa', 'Kiev', '2021-03-27', '2021-03-28', 'Odessa->Kiev');
+INSERT INTO journey (station_from, station_to, departure, arrival, route) VALUES ('Odessa', 'Kiev', '2021-03-28', '2021-03-29', 'Odessa->Kiev');
 INSERT INTO journey (station_from, station_to, departure, arrival, route) VALUES ('Kiev', 'Odessa', '2021-03-26', '2021-03-27', 'Kiev->Odessa');
-INSERT INTO journey (station_from, station_to, departure, arrival, route) VALUES ('Lviv', 'Kiev', '2021-03-24', '2021-03-25', 'Lviv->Kiev');
-INSERT INTO journey (station_from, station_to, departure, arrival, route) VALUES ('Lviv', 'Kiev', '2021-03-25', '2021-03-26', 'Lviv->Kiev');
+INSERT INTO journey (station_from, station_to, departure, arrival, route) VALUES ('Kiev', 'Odessa', '2021-03-27', '2021-03-28', 'Kiev->Odessa');
+INSERT INTO journey (station_from, station_to, departure, arrival, route) VALUES ('Kiev', 'Odessa', '2021-03-28', '2021-03-29', 'Kiev->Odessa');
 INSERT INTO journey (station_from, station_to, departure, arrival, route) VALUES ('Lviv', 'Kiev', '2021-03-26', '2021-03-27', 'Lviv->Kiev');
+INSERT INTO journey (station_from, station_to, departure, arrival, route) VALUES ('Lviv', 'Kiev', '2021-03-27', '2021-03-28', 'Lviv->Kiev');
+INSERT INTO journey (station_from, station_to, departure, arrival, route) VALUES ('Lviv', 'Kiev', '2021-03-28', '2021-03-29', 'Lviv->Kiev');
