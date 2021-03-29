6f13c1370b1e43374c2b6e9a611036d52faf640f
tango-del <53480103+tango-del@users.noreply.github.com>
2021-03-29 11:17:37 +0300

    update sql

--- src/main/resources/console.sql
+++ src/main/resources/console.sql
@@ -14,12 +14,12 @@ create table journey
 
 alter table journey owner to postgres;
 
-INSERT INTO journey (station_from, station_to, departure, arrival, route) VALUES ('Odessa', 'Kiev', '2021-03-28', '2021-03-29', 'Odessa->Kiev');
 INSERT INTO journey (station_from, station_to, departure, arrival, route) VALUES ('Odessa', 'Kiev', '2021-03-29', '2021-03-30', 'Odessa->Kiev');
 INSERT INTO journey (station_from, station_to, departure, arrival, route) VALUES ('Odessa', 'Kiev', '2021-03-30', '2021-03-31', 'Odessa->Kiev');
-INSERT INTO journey (station_from, station_to, departure, arrival, route) VALUES ('Kiev', 'Odessa', '2021-03-28', '2021-03-29', 'Kiev->Odessa');
+INSERT INTO journey (station_from, station_to, departure, arrival, route) VALUES ('Odessa', 'Kiev', '2021-03-31', '2021-04-01', 'Odessa->Kiev');
 INSERT INTO journey (station_from, station_to, departure, arrival, route) VALUES ('Kiev', 'Odessa', '2021-03-29', '2021-03-30', 'Kiev->Odessa');
 INSERT INTO journey (station_from, station_to, departure, arrival, route) VALUES ('Kiev', 'Odessa', '2021-03-30', '2021-03-31', 'Kiev->Odessa');
-INSERT INTO journey (station_from, station_to, departure, arrival, route) VALUES ('Lviv', 'Kiev', '2021-03-28', '2021-03-29', 'Lviv->Kiev');
+INSERT INTO journey (station_from, station_to, departure, arrival, route) VALUES ('Kiev', 'Odessa', '2021-03-31', '2021-04-01', 'Kiev->Odessa');
 INSERT INTO journey (station_from, station_to, departure, arrival, route) VALUES ('Lviv', 'Kiev', '2021-03-29', '2021-03-30', 'Lviv->Kiev');
 INSERT INTO journey (station_from, station_to, departure, arrival, route) VALUES ('Lviv', 'Kiev', '2021-03-30', '2021-03-31', 'Lviv->Kiev');
+INSERT INTO journey (station_from, station_to, departure, arrival, route) VALUES ('Lviv', 'Kiev', '2021-03-31', '2021-04-01', 'Lviv->Kiev');
