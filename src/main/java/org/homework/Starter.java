a69460d6888a378231a87971f7222b2ad0708d34
tango-del <53480103+tango-del@users.noreply.github.com>
2021-04-06 12:23:52 +0300

    clean unused code from TicketClient

--- src/main/java/org/homework/Starter.java
+++ src/main/java/org/homework/Starter.java
@@ -60,7 +60,7 @@ public class Starter {
 
         //System.out.println("create stop with id = " + ticketClient.createStop(stopEntity));
 
-        System.out.println("create journey with id = " + ticketClient.createJourney(journeyEntity));
+        System.out.println("create journey with id = " + ticketClient.createOrUpdateJourney(journeyEntity));
 
         final Optional<JourneyEntity> journeyById = ticketClient.getJourneyById(journeyEntity.getId(), true);
 
@@ -72,105 +72,105 @@ public class Starter {
 
         System.out.println("update journey");
 
-        ticketClient.saveJourney(journey);
+        ticketClient.createOrUpdateJourney(journey);
 
         ((AnnotationConfigApplicationContext) applicationContext).close();
     }
 
-    private static void createJourneyEntityAndSaveToDatabaseWithHibernate() {
-        final ApplicationContext applicationContext = new AnnotationConfigApplicationContext(RootConfig.class);
-
-        TicketClient ticketClient = applicationContext.getBean(TicketClient.class);
+//    private static void createJourneyEntityAndSaveToDatabaseWithHibernate() {
+//        final ApplicationContext applicationContext = new AnnotationConfigApplicationContext(RootConfig.class);
+//
+//        TicketClient ticketClient = applicationContext.getBean(TicketClient.class);
+////        System.out.println(ticketClient.find("Odessa", "Kiev", LocalDate.now(), LocalDate.now().plusDays(1)));
+//
+//        JourneyEntity journeyEntity = new JourneyEntity();
+//        journeyEntity.setStationFrom("Kiev");
+//        journeyEntity.setStationTo("Odessa");
+//        journeyEntity.setDateFrom(Instant.now());
+//        journeyEntity.setDateTo(Instant.now().plusMillis(10_000_000L));
+//        journeyEntity.setDirection(DirectionType.UNKNOWN);
+//        journeyEntity.setActive(false);
+//        System.out.println("create journey with id = " + ticketClient.createJourney(journeyEntity));
+//
+//        ((AnnotationConfigApplicationContext) applicationContext).close();
+//    }
+//
+//    private static void javaAnnotationsConfigAppContextSpringInitTicketClient() {
+//        final ApplicationContext applicationContext = new AnnotationConfigApplicationContext(RootConfig.class);
+//
+//        System.out.println(">>after init in Starter.class<<");
+//
+//        TicketClient ticketClient = applicationContext.getBean(TicketClient.class);
+//
 //        System.out.println(ticketClient.find("Odessa", "Kiev", LocalDate.now(), LocalDate.now().plusDays(1)));
-
-        JourneyEntity journeyEntity = new JourneyEntity();
-        journeyEntity.setStationFrom("Kiev");
-        journeyEntity.setStationTo("Odessa");
-        journeyEntity.setDateFrom(Instant.now());
-        journeyEntity.setDateTo(Instant.now().plusMillis(10_000_000L));
-        journeyEntity.setDirection(DirectionType.UNKNOWN);
-        journeyEntity.setActive(false);
-        System.out.println("create journey with id = " + ticketClient.createJourney(journeyEntity));
-
-        ((AnnotationConfigApplicationContext) applicationContext).close();
-    }
-
-    private static void javaAnnotationsConfigAppContextSpringInitTicketClient() {
-        final ApplicationContext applicationContext = new AnnotationConfigApplicationContext(RootConfig.class);
-
-        System.out.println(">>after init in Starter.class<<");
-
-        TicketClient ticketClient = applicationContext.getBean(TicketClient.class);
-
-        System.out.println(ticketClient.find("Odessa", "Kiev", LocalDate.now(), LocalDate.now().plusDays(1)));
-
-        ((AnnotationConfigApplicationContext) applicationContext).close();
-    }
-
-    private static void autowireConstructorTicketClient() {
-        final ApplicationContext applicationContext =
-                new ClassPathXmlApplicationContext("common-beans-with-annotations.xml");
-        System.out.println("after init");
-        TicketClient ticketClient = applicationContext.getBean(TicketClient.class);
-        System.out.println(ticketClient.find("Odessa", "Kiev", LocalDate.now(), LocalDate.now().plusDays(1)));
-
-        ticketClient = applicationContext.getBean(TicketClient.class);
-        System.out.println(ticketClient.find("Odessa", "Kiev", LocalDate.now(), LocalDate.now().plusDays(1)));
-
-        ((ClassPathXmlApplicationContext) applicationContext).close();
-    }
-
-    private static void xmlWithAnnotationsBeanAppContextSpringInit() {
-        final ApplicationContext applicationContext =
-                new ClassPathXmlApplicationContext("common-beans-with-annotations.xml");
-
-        System.out.println("after init");
-        // дай нам bean у которых объекты имеют тип как JourneyService
-        JourneyService journeyService = applicationContext.getBean("inMemoryJourneyService", JourneyService.class);
-        System.out.println(journeyService.find("Odessa", "Kiev", LocalDate.now(), LocalDate.now().plusDays(1)));
-
+//
+//        ((AnnotationConfigApplicationContext) applicationContext).close();
+//    }
+//
+//    private static void autowireConstructorTicketClient() {
+//        final ApplicationContext applicationContext =
+//                new ClassPathXmlApplicationContext("common-beans-with-annotations.xml");
+//        System.out.println("after init");
+//        TicketClient ticketClient = applicationContext.getBean(TicketClient.class);
+//        System.out.println(ticketClient.find("Odessa", "Kiev", LocalDate.now(), LocalDate.now().plusDays(1)));
+//
+//        ticketClient = applicationContext.getBean(TicketClient.class);
+//        System.out.println(ticketClient.find("Odessa", "Kiev", LocalDate.now(), LocalDate.now().plusDays(1)));
+//
+//        ((ClassPathXmlApplicationContext) applicationContext).close();
+//    }
+//
+//    private static void xmlWithAnnotationsBeanAppContextSpringInit() {
+//        final ApplicationContext applicationContext =
+//                new ClassPathXmlApplicationContext("common-beans-with-annotations.xml");
+//
+//        System.out.println("after init");
+//        // дай нам bean у которых объекты имеют тип как JourneyService
+//        JourneyService journeyService = applicationContext.getBean("inMemoryJourneyService", JourneyService.class);
+//        System.out.println(journeyService.find("Odessa", "Kiev", LocalDate.now(), LocalDate.now().plusDays(1)));
+//
+////        journeyService = applicationContext.getBean("inMemoryJourneyService", JourneyService.class);
+////        System.out.println(journeyService.find("Odessa", "Kiev", LocalDate.now(), LocalDate.now().plusDays(1)));
+//    }
+//
+//    private static void javaAnnotationsConfigAppContextSpringInit() {
+//        final ApplicationContext applicationContext = new AnnotationConfigApplicationContext(RootConfig.class);
+//
+//        System.out.println("after init");
+//        // дай нам bean у которых объекты имеют тип как JourneyService
+//        JourneyService journeyService = applicationContext.getBean("inMemoryJourneyService", JourneyService.class);
+//        System.out.println(journeyService.find("Odessa", "Kiev", LocalDate.now(), LocalDate.now().plusDays(1)));
+//
+//
+//    }
+//
+//    private static void xmlBeanAppContextSpringInit() {
+//        final ApplicationContext applicationContext = new ClassPathXmlApplicationContext("common-beans.xml");
+//
+//        System.out.println("after init");
+//        // дай нам bean у которых объекты имеют тип как JourneyService
+//        JourneyService journeyService = applicationContext.getBean("inMemoryJourneyService", JourneyService.class);
+//        System.out.println(journeyService.find("Odessa", "Kiev", LocalDate.now(), LocalDate.now().plusDays(1)));
+//
 //        journeyService = applicationContext.getBean("inMemoryJourneyService", JourneyService.class);
 //        System.out.println(journeyService.find("Odessa", "Kiev", LocalDate.now(), LocalDate.now().plusDays(1)));
-    }
-
-    private static void javaAnnotationsConfigAppContextSpringInit() {
-        final ApplicationContext applicationContext = new AnnotationConfigApplicationContext(RootConfig.class);
-
-        System.out.println("after init");
-        // дай нам bean у которых объекты имеют тип как JourneyService
-        JourneyService journeyService = applicationContext.getBean("inMemoryJourneyService", JourneyService.class);
-        System.out.println(journeyService.find("Odessa", "Kiev", LocalDate.now(), LocalDate.now().plusDays(1)));
-
-
-    }
-
-    private static void xmlBeanAppContextSpringInit() {
-        final ApplicationContext applicationContext = new ClassPathXmlApplicationContext("common-beans.xml");
-
-        System.out.println("after init");
-        // дай нам bean у которых объекты имеют тип как JourneyService
-        JourneyService journeyService = applicationContext.getBean("inMemoryJourneyService", JourneyService.class);
-        System.out.println(journeyService.find("Odessa", "Kiev", LocalDate.now(), LocalDate.now().plusDays(1)));
-
-        journeyService = applicationContext.getBean("inMemoryJourneyService", JourneyService.class);
-        System.out.println(journeyService.find("Odessa", "Kiev", LocalDate.now(), LocalDate.now().plusDays(1)));
-    }
-
-    private static void xmlBeanFactorySpringInit() {
-        // поднимается контекст в рамках маленького bean factory который мало что умеет
-        final BeanFactory beanFactory = new XmlBeanFactory(new ClassPathResource("common-beans.xml"));
-        final JourneyService journeyService = beanFactory.getBean("inMemoryJourneyService", JourneyService.class);
-        System.out.println(journeyService.find("Odessa", "Kiev", LocalDate.now(), LocalDate.now().plusDays(1)));
-        System.out.println(journeyService.find("Odessa", "Kiev", LocalDate.now(), LocalDate.now().plusDays(6)));
-    }
-
-    private static void exampleBeforeSpringContext() throws IOException, IllegalAccessException, InstantiationException, ClassNotFoundException {
-        AppContext.load("application.properties");
-        final JourneyService journeyService = AppContext.getBean("journeyService");
-
-        System.out.println(journeyService.find("Odessa", "Kiev", LocalDate.now(), LocalDate.now().plusDays(1)));
-
-        // можем обернуть в какой-то Exception красивый для клиента
-        System.out.println(journeyService.find("Odessa", "Kiev", LocalDate.now(), LocalDate.now().plusDays(6)));
-    }
+//    }
+//
+//    private static void xmlBeanFactorySpringInit() {
+//        // поднимается контекст в рамках маленького bean factory который мало что умеет
+//        final BeanFactory beanFactory = new XmlBeanFactory(new ClassPathResource("common-beans.xml"));
+//        final JourneyService journeyService = beanFactory.getBean("inMemoryJourneyService", JourneyService.class);
+//        System.out.println(journeyService.find("Odessa", "Kiev", LocalDate.now(), LocalDate.now().plusDays(1)));
+//        System.out.println(journeyService.find("Odessa", "Kiev", LocalDate.now(), LocalDate.now().plusDays(6)));
+//    }
+//
+//    private static void exampleBeforeSpringContext() throws IOException, IllegalAccessException, InstantiationException, ClassNotFoundException {
+//        AppContext.load("application.properties");
+//        final JourneyService journeyService = AppContext.getBean("journeyService");
+//
+//        System.out.println(journeyService.find("Odessa", "Kiev", LocalDate.now(), LocalDate.now().plusDays(1)));
+//
+//        // можем обернуть в какой-то Exception красивый для клиента
+//        System.out.println(journeyService.find("Odessa", "Kiev", LocalDate.now(), LocalDate.now().plusDays(6)));
+//    }
 }
--- src/main/java/org/homework/service/TicketClient.java
+++ src/main/java/org/homework/service/TicketClient.java
@@ -3,15 +3,11 @@ package org.homework.service;
 import org.homework.Journey;
 import org.homework.persistence.entity.JourneyEntity;
 import org.homework.persistence.entity.StopEntity;
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
-import org.springframework.util.Assert;
 import org.springframework.util.CollectionUtils;
 
 import javax.annotation.PostConstruct;
@@ -19,67 +15,39 @@ import javax.annotation.PreDestroy;
 import java.time.LocalDate;
 import java.util.*;
 
-// клиентский сервис который будет дёргать JourneyService
 @Component
 public class TicketClient {
-//public class TicketClient implements DisposableBean { // -> без использования PreDestroy @Override destroy()
-//public class TicketClient implements InitializingBean { // -> без использования @PostConstruct @Override afterPropertiesSet()
-
-    //private JourneyService journeyService; // не будем оперировать какой-то конкретно реализацией, а интерфейсом
 
     @Autowired
     private List<JourneyService> journeyServices;
-
-    //@Autowired(required = false)
-    //@Autowired
-    //private Map<String, JourneyService> journeyServices;
 
     @Autowired
     @Qualifier("transactionalJourneyService")
     private TransactionalJourneyService transactionalJourneyService;
 
-    /*
-    system.message -> ищет property с таким названием
-    если system.message отсутствует то выведет дефолт значение после двоиточего 'default value'
-     */
-    @Value("${system.message:default value}")
-    private String systemMessage;
-//    @Autowired
-//    private Environment environment;
-
-    /*@Autowired
-    @Qualifier("inMemoryJourneyService")
-    public void setJourneyService(final JourneyService journeyService) {
-        if (journeyService == null) throw new IllegalArgumentException("journeyService must be set");
-
-        this.journeyService = journeyService;
-    }*/
-
     @Autowired
     private TransactionalStopService stopService;
 
+    @Value("${system.message:default value}")
+    private String systemMessage;
 
-    public Long createJourney(final JourneyEntity journeyEntity) {
-        if (journeyEntity == null) throw new IllegalArgumentException("journeyEntity must be set");
+    @Autowired
+    private Environment environment;
 
-//        return journeyServices.get("transactionalJourneyService").createJourney(journeyEntity);
-        return transactionalJourneyService.createJourney(journeyEntity);
-    }
-
-    public Long createStop(final StopEntity stopEntity) {
+    public StopEntity createOrUpdateStop(final StopEntity stopEntity) {
         if (stopEntity == null) throw new IllegalArgumentException("stopEntity must be set");
-        return stopService.createStop(stopEntity);
+        return stopService.createOrUpdateStop(stopEntity);
     }
 
     public Optional<JourneyEntity> getJourneyById(final Long id, boolean withDependencies) {
         //Assert.notNull(id, "id must be set");
         if (id == null) return Optional.empty();
-        return transactionalJourneyService.getById(id, withDependencies);
+        return transactionalJourneyService.findById(id, withDependencies);
     }
 
-    public void saveJourney(final JourneyEntity journey) {
+    public JourneyEntity createOrUpdateJourney(final JourneyEntity journey) {
         if (journey == null) throw new IllegalArgumentException("entity must be set");
-        transactionalJourneyService.save(journey);
+        return transactionalJourneyService.createOrUpdateJourney(journey);
     }
 
     public Collection<Journey> find(final String stationFrom, final String stationTo, final LocalDate dateFrom, final LocalDate dateTo) {
@@ -88,10 +56,6 @@ public class TicketClient {
         if (dateFrom == null) throw new IllegalArgumentException("date from must be set");
         if (dateTo == null) throw new IllegalArgumentException("date to must be set");
 
-        // -> private JourneyService journeyService;
-//        return journeyService.find(stationFrom, stationTo, dateFrom, dateTo);
-
-        // -> private List<JourneyService> journeyServices;
         for (JourneyService service : journeyServices) {
             System.out.println(service);
 
@@ -100,28 +64,7 @@ public class TicketClient {
             if (!CollectionUtils.isEmpty(journeys)) return journeys;
         }
 
-        // -> private Map<String, JourneyService> journeyServices;
-        //Collection<Journey> service = journeyServices.get("inMemoryJourneyService").find(stationFrom, stationTo, dateFrom, dateTo);
-
-//        if (service == null) {
-//            return Collections.emptyList();
-//        }
-//
-//        return service;
-
         return Collections.emptyList();
-    }
-
-    public static void main(String[] args) {
-
-        final JourneyService journeyService = new InMemoryJourneyServiceImpl();
-//        final TicketClient client = new TicketClient(journeyService);
-        final TicketClient client = new TicketClient();
-
-        System.out.println(client.find("Odessa", "Kiev", LocalDate.now(), LocalDate.now().plusDays(1)));
-
-        // можем оберуть в какой-то Exception красивый для клиента
-        System.out.println(client.find("Odessa", "Kiev", LocalDate.now(), LocalDate.now().plusDays(6)));
     }
 
     @PostConstruct
@@ -141,6 +84,4 @@ public class TicketClient {
     public void destroy() throws Exception {
 //        System.out.println("destroy bean in method destroy() class TicketClient");
     }
-
-
 }
