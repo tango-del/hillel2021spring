76cc5f01f34dcc8a93d69d6440ac923fd23dad9b
tango-del <53480103+tango-del@users.noreply.github.com>
2021-04-20 13:05:14 +0300

    console.sql add sql for stored procedures

--- src/main/resources/console.sql
+++ src/main/resources/console.sql
@@ -1,21 +1,47 @@
-create schema if not exists database_journey_service;
 
-create table database_journey_service.journey
-(
-	id serial,
-	station_from varchar(50) not null,
-	station_to varchar(50) not null,
-	departure date not null ,
-	arrival date not null ,
-	route varchar(200) not null
-);
+create or replace FUNCTION find_all_vehicles() returns refcursor as
+$$
+declare
+vehicle_cursor refcursor;
+begin
+open vehicle_cursor for SELECT * from vehicle;
+return vehicle_cursor;
+end;
+$$ language plpgsql;
 
-INSERT INTO database_journey_service.journey (station_from, station_to, departure, arrival, route) VALUES ('Odessa', 'Kiev', '2021-03-16', '2021-03-17', 'Odessa->Kiev');
-INSERT INTO database_journey_service.journey (station_from, station_to, departure, arrival, route) VALUES ('Odessa', 'Kiev', '2021-03-17', '2021-03-18', 'Odessa->Kiev');
-INSERT INTO database_journey_service.journey (station_from, station_to, departure, arrival, route) VALUES ('Odessa', 'Kiev', '2021-03-18', '2021-03-19', 'Odessa->Kiev');
-INSERT INTO database_journey_service.journey (station_from, station_to, departure, arrival, route) VALUES ('Kiev', 'Odessa', '2021-03-16', '2021-03-17', 'Kiev->Odessa');
-INSERT INTO database_journey_service.journey (station_from, station_to, departure, arrival, route) VALUES ('Kiev', 'Odessa', '2021-03-17', '2021-03-18', 'Kiev->Odessa');
-INSERT INTO database_journey_service.journey (station_from, station_to, departure, arrival, route) VALUES ('Kiev', 'Odessa', '2021-03-18', '2021-03-19', 'Kiev->Odessa');
-INSERT INTO database_journey_service.journey (station_from, station_to, departure, arrival, route) VALUES ('Lviv', 'Kiev', '2021-03-16', '2021-03-17', 'Lviv->Kiev');
-INSERT INTO database_journey_service.journey (station_from, station_to, departure, arrival, route) VALUES ('Lviv', 'Kiev', '2021-03-17', '2021-03-18', 'Lviv->Kiev');
-INSERT INTO database_journey_service.journey (station_from, station_to, departure, arrival, route) VALUES ('Lviv', 'Kiev', '2021-03-18', '2021-03-19', 'Lviv->Kiev');
+
+begin;
+select find_all_vehicles();
+fetch all in "<unnamed portal 1>";
+end;
+commit;
+
+-- Не безопасно
+create or replace FUNCTION find_all(p_db_name IN varchar(20)) returns refcursor as
+$$
+declare
+db_cursor refcursor;
+    l_sql varchar(1000);
+begin
+    l_sql := 'select * from ' || p_db_name;
+open db_cursor for execute l_sql;
+return db_cursor;
+end;
+$$ language plpgsql;
+
+-- Более безопасно
+create or replace FUNCTION find_all(p_db_name IN varchar(20)) returns refcursor as
+$$
+declare
+db_cursor refcursor;
+begin
+open db_cursor for execute format('select * from %I', p_db_name);
+return db_cursor;
+end;
+$$ language plpgsql;
+
+begin;
+select find_all('stop');
+fetch all in "<unnamed portal 9>";
+end;
+commit;
